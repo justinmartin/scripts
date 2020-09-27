df5d752f12b9c1a483056d04921879257b1243f9
Justin Martin <jus10mar10@gmail.com>
2020-09-27 15:09:51 -0500

    enable and start SSH, install, enable and start QEMU Guest Agent

--- arch_setup.sh
+++ arch_setup.sh
@@ -5,6 +5,9 @@
 pacman -Syy
 pacman -S git openssh fish --noconfirm
 
+systemctl enable sshd
+systemctl start sshd
+
 pacman -S base-devel gcc --noconfirm
 
 # Install and Configure sudo
@@ -15,6 +18,13 @@ echo '%wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
 # Install Docker-Compose
 
 pacman -S docker docker-compose --noconfirm
+systemctl enable docker
+systemctl start docker
+
+pacman -S qemu-guest-agent
+systemctl enable qemu-ga
+systemctl start qemu-ga
+
 
 # Create User Account
 useradd --create-home --shell /usr/bin/fish --groups docker,wheel justin 
